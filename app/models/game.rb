93d95c062c3de3a970e53f080e4c115a3a486c07
bobcheese2531 <test@example.com>
2021-04-24 08:42:07 +0000

    Gameモデルのアソシエーションとバリデーション設定

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,16 @@
 class Game < ApplicationRecord
+    #アソシエーション
+    belongs_to :user, optional: true
+    has_many :rates
+    
+    
+    #バリデーション
+    validates :home_team, :presence => true, length: { is: 3 }  #チーム名3文字
+    validates :away_team, :presence => true, length: { is: 3 }
+    validates :home_score, :presence => true, numericality: { only_integer: true }  #数字のみ許可
+    validates :away_score, :presence => true, numericality: { only_integer: true }
+    validates :date, :presence => true
+    validates :game_text, :presence => true, length: { maximum: 150 }   #最大150文字
+    validates :rate_team, :presence => true, length: { is: 3 }
+    validates :youtube_url, allow_blank: true   #空でも保存可能
 end
