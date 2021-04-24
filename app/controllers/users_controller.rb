3930fd044776d0c0b3c9f99d2dfbfb61a6146dbe
bobcheese2531 <test@example.com>
2021-04-24 13:22:34 +0000

    usersコントローラー設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: %i[ show ]
+    
+    # GET /users/id
+    def show
+    end
+    
+    # GET /users/id/edit
+    def edit
+    end
+    
+    # PATCH/PUT /users/id
+    def update
+    end
+    
+    
+    private
+    
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
+    def user_params
+        params.require(:user).permit(:name, :image, :user_text, :like_team)
+    end
+    
 end
