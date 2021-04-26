4bcbf8ffe4bfde13863c30bf24736cec97114ea5
bobcheese2531 <test@example.com>
2021-04-26 08:41:15 +0000

    プロフィール編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+    
+    #before_action
     before_action :set_user, only: %i[ show edit update ]
+    before_action :authenticate_user!, except: [:show]
     
     # GET /users/id
     def show
@@ -11,17 +14,21 @@ class UsersController < ApplicationController
     
     # PATCH/PUT /users/id
     def update
+      if @user.id == current_user.id
+        @user.update(user_params)
+      end
+      redirect_to root_path
     end
     
     
     private
     
     def set_user
-        @user = User.find(params[:id])
+      @user = User.find(params[:id])
     end
     
     def user_params
-        params.require(:user).permit(:name, :image, :user_text, :like_team)
+      params.require(:user).permit(:name, :image, :user_text, :like_team)
     end
     
 end
