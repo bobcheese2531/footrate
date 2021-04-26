86131dc12f04029c684207e08770bd4bc6d42af8
bobcheese2531 <test@example.com>
2021-04-26 06:29:09 +0000

    userモデルのアソシエーションとバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  #アソシエーション
+  has_many :games
+  
+  #バリデーション
+  validates :name, presence: true, uniqueness: true #記入必須、'name'は被り禁止
+  
   # ユーザーのプロフィール画像を表示
   mount_uploader :image, ImageUploader
+  
 end
