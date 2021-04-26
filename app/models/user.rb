17b6d2cad709e01b4ea0e6d939c2acc4d235955b
bobcheese2531 <test@example.com>
2021-04-26 06:24:17 +0000

    新規登録フォーム作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  # ユーザーのプロフィール画像を表示
+  mount_uploader :image, ImageUploader
 end
