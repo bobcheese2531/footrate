cce9620e5afaef8b264462d9bc8060caad3965d6
bobcheese2531 <test@example.com>
2021-05-05 06:51:17 +0000

    認証メール機能修正2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   
-  host = "footrate.herokuapp.com"
+  host = "#{ENV['HEROKU_APPNAME']}.herokuapp.com"
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   
   config.action_mailer.smtp_settings = {
