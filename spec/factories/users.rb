53d56d1e05b1ab1f11cc6fb255e9d55d95980301
bobcheese2531 <test@example.com>
2021-05-26 16:38:58 +0000

    ユーザーのダミーデータ作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"footrate"}
+    name                  {"footrate"}
     email                 {"test@gmail.com"}
     password              {"12345678"}
     password_confirmation {"12345678"}
