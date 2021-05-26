89c4e1cf8f214f54643e4ccfc01fc915b8c58fee
bobcheese2531 <test@example.com>
2021-05-26 16:37:35 +0000

    ユーザーのダミーデータ作成

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -1,9 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Rate, type: :model do
-  it "ポジション, 選手名, 感想, 評価がある場合有効" do
-    game = Game.new(id: 1)
-    rate = create(d: 1, game_id: game.id, position: "CMF", player_name: "KDB", player_text: "最高だった", player_rate: 10)
-    expect(rate).to be_valid
-  end  
 end
