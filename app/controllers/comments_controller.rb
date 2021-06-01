b0f36fa9527b5cfcbdf8d071be4d1df140f6a9f7
bobcheese2531 <test@example.com>
2021-06-01 06:14:04 +0000

    コード整理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,11 @@ class CommentsController < ApplicationController
      # POST /game/id/comment
     def create
       @comment = Comment.new(comment_params)
-      @comment.save if @comment.user_id = current_user.id
-      redirect_back(fallback_location: root_path)
+      redirect_back(fallback_location: root_path) if @comment.save
     end
     
     private
     def comment_params
-        params.require(:comment).permit(:comment).merge(game_id: params[:game_id])
+      params.require(:comment).permit(:comment).merge(user_id: current_user.id, game_id: params[:game_id])
     end
 end
