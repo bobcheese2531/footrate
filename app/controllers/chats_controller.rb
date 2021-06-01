181677fc42baf323d3996af31c27867bc32f2f78
bobcheese2531 <test@example.com>
2021-06-01 07:08:14 +0000

    コード整理

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,8 +1,14 @@
 class ChatsController < ApplicationController
-    
+  
+  before_action :authenticate_user! 
+  
   def show
     find_room
-    set_or_create_room
+    unless @user_rooms.nil?
+      set_room
+    else
+      create_room
+    end
     set_chats
   end
     
@@ -24,20 +30,20 @@ class ChatsController < ApplicationController
     @user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
   end
   
-  def set_or_create_room
-    unless @user_rooms.nil?
-      @room = @user_rooms.room
-    else
-      @room = Room.new
-      @room.save
-      UserRoom.create(user_id: current_user.id, room_id: @room.id)
-      UserRoom.create(user_id: @user.id, room_id: @room.id)
-    end
+  def set_room
+    @room = @user_rooms.room
+  end
+  
+  def create_room
+    @room = Room.new
+    @room.save
+    UserRoom.create(user_id: current_user.id, room_id: @room.id)
+    UserRoom.create(user_id: @user.id, room_id: @room.id)
   end
   
   def set_chats
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
-    
+  
 end
