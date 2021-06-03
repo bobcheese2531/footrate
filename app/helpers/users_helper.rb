f108eb59547e35a142dcb2b9b32b91dc376f1f3e
bobcheese2531 <test@example.com>
2021-06-03 09:49:33 +0000

    リファクタリング実施

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,25 +1,35 @@
 module UsersHelper
   
   def profile_image(user)
+    
     if user.image?
       image = content_tag(
         :img,
-        "",
+        "#{user.name}",
         src: "#{user.image}",
-        height: 150,
-        width: 150
+        height: 65,
+        width: 65
         )
-      content_tag(:td, image)
+      content_tag(:a, image, href: "/users/#{user.id}")
+      
     else
+      
       image = content_tag(
         :img,
-        "",
+        "#{user.name}",
         src: "https://4.bp.blogspot.com/-xz7m7yMI-CI/U1T3vVaFfZI/AAAAAAAAfWI/TOJPmuapl-c/s800/figure_standing.png",
-        height: 150,
-        width: 150
+        height: 65,
+        width: 65
         )
-      content_tag(:td, image)
+      content_tag(:a, image, href: "/users/#{user.id}")
+      
     end
+    
   end
+  
+  def other_user?(user)
+    user_signed_in? && current_user != user
+  end
+  
   
 end
