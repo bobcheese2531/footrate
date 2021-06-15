11c0b8846a2ff042a59c434109bdcb6b871b9fc8
bobcheese2531 <test@example.com>
2021-06-15 07:09:35 +0000

    apiモジュール作成

---
+++ lib/api/football_data/request.rb
@@ -0,0 +1,28 @@
+module Api
+  module FootballData
+    class Request
+
+      def initialize
+        code = code
+        id = id
+      end
+  
+      def self.get_games(code)
+        url = "https://api.football-data.org/v2/competitions/#{code}/matches"
+        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] }
+        client = HTTPClient.new
+        response = client.get(url, header: header)
+        JSON.parse(response.body)
+      end
+      
+      def self.show_game(id)
+        url = "https://api.football-data.org/v2/matches/#{id}"
+        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] } 
+        client = HTTPClient.new
+        response = client.get(url, header: header)
+        JSON.parse(response.body)
+      end
+
+    end
+  end
+end
