fc027377c63ebc7730c024ffd8416935800b90b5
bobcheese2531 <test@example.com>
2021-06-15 07:20:05 +0000

    gamesコントローラー修正

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,70 +1,38 @@
 class GamesController < ApplicationController
-    
-    #before_action
-    before_action :authenticate_user!, except: [:index, :show]  #index, show以外はログインを必要とする
-    before_action :set_game, only: %i[ show edit update destroy ]
-    
-    # GET /games
-    def index
-      @games = Game.get_games(params[:page], params[:search])
-    end
-    
-    # GET /games/id
-    def show
-      @title = "#{@game.home_team} vs #{@game.away_team}"
-      set_comment_like
-    end
-    
-    # GET /games/new
-    def new
-      @game = Game.new
-      16.times{ @game.rates.build }   #選手用のフォームを16（人数分）用意
-      @submit = "投稿する"
-    end
-    
-    # GET /games/id/edit
-    def edit
-      redirect_to action: :index unless same_user?
-      @submit = "編集する"
-    end
-    
-    # POST /games
-    def create
-      @game = Game.new(game_params)
-      @game.save ? (redirect_to action: :index) : (render "new")
-    end
-    
-    # PATCH/PUT /games/id
-    def update
-      @game.update(game_params) if same_user?
-      redirect_to action: :index
-    end
-    
-    # DELETE /games/id
-    def destroy
-      @game.destroy if @game.same_user?
-      redirect_to action: :index
-    end
-    
-    private
-    def set_game
-      @game = Game.find(params[:id])
-    end
-    
-    def game_params
-      params.require(:game).permit(:home_team, :away_team, :home_score, :away_score, :date, :game_text, :rate_team, :youtube_url,
-      rates_attributes: [:game_id, :position, :player_name, :player_rate, :player_text, :id, :_destroy])  #複数モデルに同時保存用
-      .merge(user_id: current_user.id)
-    end
-    
-    def same_user?
-      @game.user_id == current_user.id
-    end
-    
-    def set_comment_like
-      @comments = @game.comments  #表示するよう
-      @comment = Comment.new  #新規投稿用
-      @like = Like.new    #いいね
-    end
+  require 'json'
+  require 'httpclient'
+  
+  # GET /games
+  def index
+  end
+
+  # GET /games/1 
+  def show
+    id = params["id"]
+    result= Api::FootballData::Request.show_game(id)
+    @home = result["match"]["homeTeam"]["lineup"]
+    @away = result["match"]["awayTeam"]["lineup"]
+    @game = Game.find_or_create_by(id: id)
+  end
+  
+  def cl
+    result= Api::FootballData::Request.get_games("CL")
+    @match = result["matches"]
+  end
+  
+  def pl
+    result= Api::FootballData::Request.get_games("PL")
+    @match = result["matches"]
+  end
+  
+  def liga
+    result= Api::FootballData::Request.get_games("PD")
+    @match = result["matches"]
+  end
+  
+  def euro
+    result= Api::FootballData::Request.get_games("EC")
+    @match = result["matches"]
+  end
   
 end
