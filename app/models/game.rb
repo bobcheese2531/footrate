0da07a109c4c28c98d42e4969788d66c799c4009
bobcheese2531 <test@example.com>
2021-06-15 06:58:02 +0000

    アソシエーション修正

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,27 +1,7 @@
 class Game < ApplicationRecord
     
   #アソシエーション
-  belongs_to :user
   has_many :rates, dependent: :destroy
-  accepts_nested_attributes_for :rates, allow_destroy: true 
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :liked_users, through: :likes, source: :user
-    
-  #バリデーション
-  with_options presence: true do
-    validates :date
-    with_options numericality: { only_integer: true } do  #数字のみ許可
-      validates :home_score
-      validates :away_score
-    end
-    with_options length: { is: 3 } do #3文字のみ保存可能
-      validates :rate_team
-      validates :home_team
-      validates :away_team
-    end
-  end
-  validates :game_text, length: { maximum: 150 }
   
   #検索機能
   def self.search(search)
--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,19 +1,8 @@
 class Rate < ApplicationRecord
   
-  #アソシエーション
   belongs_to :game
+  belongs_to :user
     
-  #バリデーション
-  with_options presence: true do
-    validates :player_name
-    validates :position, length: { maximum: 3 } #最大3文字
-    validates :player_text, length: { maximum: 100 } #最大100文字
-    validates :player_rate, numericality: { only_integer: true } #数字のみ保存可能に
-  end
+  has_many :players
   
-  enum position: {'GK': 'GK', 'CB': 'CB', 'RB': 'RB', 'LB': 'LB', 'DMF': 'DMF', 'CMF': 'CMF', 'OMF': 'OMF', 
-                  'RMF': 'RMF', 'LMF': 'LMF', 'RWG': 'RWG', 'LWG': 'LWG', 'ST': 'ST', 'CF': 'CF' }
-                  
-  enum player_rate: {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, 
-                    '6': 6, '7': 7, '8': 8, '9': 9, '10': 10}
 end
