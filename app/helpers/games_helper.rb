89368135426fbc642040f1bc5023b72374c5397d
bobcheese2531 <test@example.com>
2021-06-18 16:53:33 +0000

    ビュー修正

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,20 +1,2 @@
 module GamesHelper
-  
-  def score(game)
-    "#{game.home_team} #{game.home_score} : #{game.away_score} #{game.away_team}"
-  end
-  
-  def youtube(game)
-    if game.youtube_url?
-      iframe = content_tag(
-      :iframe,
-      "",
-      src: "https://www.youtube.com/embed/#{game.youtube_url.last(11)}",
-      frameborder: "0",
-      allowfullscreen: true
-      )
-      content_tag(:td, iframe)
-    end 
-  end
-  
 end
