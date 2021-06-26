96f6a476bdea73ca8403debdc804ac5b7dc61ba5
bobcheese2531 <test@example.com>
2021-06-26 09:33:40 +0000

    フォームにサブの選手表示

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,6 +4,8 @@ class PlayersController < ApplicationController
   
   def new
     set_match(params[:game_id])
+    @home = set_members(@match['homeTeam'])
+    @away = set_members(@match['awayTeam'])
     rate = Rate.find_or_create_by(game_id: params[:game_id], user_id: current_user.id, home_team: @match["homeTeam"]["name"],away_team: @match["awayTeam"]["name"])
     @player = Form::PlayerCollection.new(rate_id: rate.id)
   end
@@ -17,6 +19,14 @@ class PlayersController < ApplicationController
   def player_collection_params
     params.require(:form_player_collection)
     .permit(players_attributes: [:name, :player_rate, :rate_text, :position, :shirtnumber, :rate_id, :team_id])
+  end
+  
+  def set_members(team)
+    sub = @match['substitutions'].select {|s| s['team']['name'] == team['name'] } 
+    sub.each do |s|
+      team['lineup'] << { "name" => s["playerIn"]["name"], "position" => "SUB", "shirtNumber" => 0 }
+    end
+    team['lineup']
   end
   
 end
