21cd241d6f4533aacc4421a6559383514bd2fef0
bobcheese2531 <test@example.com>
2021-06-29 13:26:53 +0000

    レスポンシブ対応

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -7,6 +7,22 @@ module GamesHelper
   def get_time(time)
     time.slice(11..15)
   end
+  
+  def scorer?(scorer, player)
+    "G" if scorer == player
+  end
+  
+  def player_in?(sub, b)
+    "#{sub["minute"]}' ↑" if b["name"] == sub["playerIn"]["name"]
+  end
+  
+  def player_out?(sub, l)
+    "#{sub["minute"]}' ↓" if l["name"] == sub["playerOut"]["name"]
+  end
+  
+  def goal_team?(goal, team)
+    "#{goal["minute"]}' #{goal["scorer"]["name"]}" if goal["team"]["name"] == team
+  end
     
   
 end
