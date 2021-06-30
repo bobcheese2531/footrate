30f8022560df74b913a411a691e86d4b4d3033d5
bobcheese2531 <test@example.com>
2021-06-30 08:08:32 +0000

    APIコードリファクタリング

--- lib/api/football_data/request.rb
+++ lib/api/football_data/request.rb
@@ -1,75 +1,56 @@
 module Api
   module FootballData
     class Request
-
-      def initialize
-      end
+      
+      @url = "https://api.football-data.org/v2"
       
       def self.get_scheduled_games
-        url = "https://api.football-data.org/v2/competitions/EC/matches?stage=LAST_16"
-        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] }
-        client = HTTPClient.new
-        response = client.get(url, header: header)
-        JSON.parse(response.body)
+        url = "#{@url}/competitions/EC/matches?stage=QUARTER_FINAL"
+        fetch_data(url)
       end
   
       def self.get_league_games(code, year, day)
-        url = "https://api.football-data.org/v2/competitions/#{code}/matches?season=#{year}&matchday=#{day}"
-        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] }
-        client = HTTPClient.new
-        response = client.get(url, header: header)
-        JSON.parse(response.body)
+        url = "#{@url}/competitions/#{code}/matches?season=#{year}&matchday=#{day}"
+        fetch_data(url)
       end
       
       def self.get_tournament_games(code, year, stage, day)
-        url = "https://api.football-data.org/v2/competitions/#{code}/matches?season=#{year}&stage=#{stage}&matchday=#{day}"
-        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] }
-        client = HTTPClient.new
-        response = client.get(url, header: header)
-        JSON.parse(response.body)
+        url = "#{@url}/competitions/#{code}/matches?season=#{year}&stage=#{stage}&matchday=#{day}"
+        fetch_data(url)
       end
       
       def self.show_game(id)
-        url = "https://api.football-data.org/v2/matches/#{id}"
-        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] } 
-        client = HTTPClient.new
-        response = client.get(url, header: header)
-        JSON.parse(response.body)
+        url = "#{@url}/matches/#{id}"
+        fetch_data(url)
       end
       
       def self.get_standings(code)
-        url = "https://api.football-data.org/v2/competitions/#{code}/standings"
-        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] } 
-        client = HTTPClient.new
-        response = client.get(url, header: header)
-        JSON.parse(response.body)
+        url = "#{@url}/competitions/#{code}/standings"
+        fetch_data(url)
       end
       
       def self.get_scorers(code, year)
-        url = "https://api.football-data.org/v2/competitions/#{code}/scorers?season=#{year}"
-        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] } 
-        client = HTTPClient.new
-        response = client.get(url, header: header)
-        JSON.parse(response.body)
+        url = "#{@url}/competitions/#{code}/scorers?season=#{year}"
+        fetch_data(url)
       end
       
       def self.get_teams
-          url = "https://api.football-data.org/v2/teams?areas=2114,2088,2081,2224,2072"
-          header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] } 
-          client = HTTPClient.new
-          response = client.get(url, header: header)
-          JSON.parse(response.body)
+          url = "#{@url}/teams?areas=2114,2088,2081,2224,2072"
+          fetch_data(url)
       end
       
       def self.get_team_matches(id)
-        url = "https://api.football-data.org/v2/teams/#{id}/matches?limit=5"
-        header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] } 
-        client = HTTPClient.new
-        response = client.get(url, header: header)
-        JSON.parse(response.body)
+        url = "#{@url}/teams/#{id}/matches?limit=5"
+        fetch_data(url)
       end
       
-      
+      private
+      def self.fetch_data(url)
+        @header = { 'X-Auth-Token': ENV['FOOTBALL_DATA_API_KEY'] }
+        @client = HTTPClient.new
+        response = @client.get(url, header: @header)
+        JSON.parse(response.body)
+      end
 
     end
   end
